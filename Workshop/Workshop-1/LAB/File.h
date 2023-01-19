88034fcc87a16f09e777e8569da8a04a85b27345
runner-jaimie <jlee723@myseneca.ca>
2023-01-19 16:12:18 -0500

    OOP244 Week1 LAB Workshop

---
+++ Workshop/Workshop-1/LAB/File.h
@@ -0,0 +1,16 @@
+#ifndef SDDS_FILE_H
+#define SDDS_FILE_H
+
+namespace sdds
+{
+
+  bool openFile(const char filename[]);
+  void closeFile();
+  bool readTitle(char title[]);
+  bool readYear(int* year);
+  bool readMovieRating(char rating[]);
+  bool readDuration(int* duration);
+  bool readGenres(char genres[][11]);
+  bool readConsumerRating(float* rating);
+}
+#endif
