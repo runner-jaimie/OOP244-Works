71019855f30fb5a83f2d958d0555790e370907bf
runner-jaimie <jlee723@myseneca.ca>
2023-01-27 16:42:29 -0500

    Workshop-2

---
+++ Workshop/Workshop-2/LAB/LAB/Package.h
@@ -0,0 +1,36 @@
+#ifndef SDDS_PACKAGE_H_
+#define SDDS_PACKAGE_H_
+
+#define filename_r "Filtered_Dataset.csv"
+
+namespace sdds {
+
+    struct Customers {
+        char* Package_Name;
+        int user_id;
+        double timeinhours;
+        int dayofyear;
+        int Fwifitime;
+        int Fctime;
+        char dayofweek;
+    };
+    // loads a customers structure with its values from the file
+    int loadTraces();
+
+    // allocates dynamic array of customers and loads all the file records into the array.
+    int loadTraces(Customers& user);
+    
+    // user id in ascending
+    void grouptTraces(); //Group the dynamic array of records based on the user id of customers.
+
+    // ADD [1]: Declare a prototype for the display function, this function displays a customer record on the screen.
+    void display();
+
+    // ADD [2]: Declare a prototype for the deallocateMemory function that do the following:
+    // (1). deallocates all the Package names in the customer (users) elements
+    // (2). deallocates the users array
+    void dellocagteMemory();
+
+
+}
+#endif  
