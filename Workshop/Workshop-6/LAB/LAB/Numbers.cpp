5b40483754b09f8fa1572d4104546cc484256321
runner-jaimie <jlee723@myseneca.ca>
2023-04-03 12:49:08 -0400

    Editing Number.cpp

--- Workshop/Workshop-6/LAB/LAB/Numbers.cpp
+++ Workshop/Workshop-6/LAB/LAB/Numbers.cpp
@@ -15,15 +15,15 @@ namespace sdds
       double temp;
       for(i = size - 1; size && i > 0; i--)
       {
-         for(j = 0; j < i; j++)
-         {
-            if(nums[j] > nums[j + 1])
-            {
-               temp = nums[j];
-               nums[j] = nums[j + 1];
-               nums[j + 1] = temp;
-            }
-         }
+        for(j = 0; j < i; j++)
+        {
+          if(nums[j] > nums[j + 1])
+          {
+              temp = nums[j];
+              nums[j] = nums[j + 1];
+              nums[j + 1] = temp;
+          }
+        }
       }
     }
 
