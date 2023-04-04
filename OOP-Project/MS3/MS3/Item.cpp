77e42a44abba95082074a6d65fbb724c4881c3e0
runner-jaimie <jlee723@myseneca.ca>
2023-04-03 21:41:03 -0400

    Fixing error

--- OOP-Project/MS3/MS3/Item.cpp
+++ OOP-Project/MS3/MS3/Item.cpp
@@ -384,14 +384,14 @@ namespace sdds
            cout << "> ";
            //istr.ignore(1000, '\n');
            istr >> m_quantity;
-           if(!istr || m_quantity >= MAX_STOCK_NUMBER + 1 || m_quantity <= 0)
+           if(!istr || m_quantity >= MAX_STOCK_NUMBER + 1 || m_quantity == 0)
            {
                if(!istr)
                {
                    istr.clear();
-                   istr.ignore(1000, '\n');
+                   //istr.ignore(1000, '\n');
                }
-               //istr.ignore(1000, '\n');
+               istr.ignore(1000, '\n');
                ok = false;
                cout << ERROR_POS_QTY << endl;
            }
